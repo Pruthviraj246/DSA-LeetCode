4f80ae1b001b84d2346df3e95cc3c1d18c368bca
Pruthviraj <pruthvirajmurthy79@gmail.com>
2026-07-28 23:48:28 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.95%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -2,9 +2,9 @@ class Solution {
     public int reverseBits(int n) {
         int reverse=0;
         for(int i=0;i<32;i++){
-            reverse <<= 1;
-            reverse |= (n&1);
-            n >>= 1;
+            reverse<<=1;
+            reverse|=(n&1);
+            n>>=1;
         }
         return reverse;
     }

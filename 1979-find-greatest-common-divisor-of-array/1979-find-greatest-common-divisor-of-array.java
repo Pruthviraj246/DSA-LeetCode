c34190be2d2263aa2c194389671ff20eec7cad8d
Pruthviraj <pruthvirajmurthy79@gmail.com>
2026-08-09 22:44:45 +0530

    Time: 0 ms (100%), Space: 44.7 MB (97.86%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,7 +1,17 @@
 class Solution {
     public int findGCD(int[] nums) {
-        int max=max(nums);
-        int min=min(nums);
+        int max=nums[0];
+        int min=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+
+            if(nums[i]<min){
+                min=nums[i];
+            }      
+        }
+
         int gcd=gcd(max,min);
         return gcd;
     }
@@ -13,23 +23,40 @@ class Solution {
         return gcd(b,a%b);
     }
 
-    static int max(int[] arr){
-        int max=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>max){
-            max=arr[i];
-            }
-        }
-        return max;
-    }
-
-    static int min(int[] arr){
-        int min=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<min){
-            min=arr[i];
-            }
-        }
-        return min;
-    }
 }
+
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         int max=max(nums);
+//         int min=min(nums);
+//         int gcd=gcd(max,min);
+//         return gcd;
+//     }
+
+//     static int gcd(int a,int b){
+//         if(b==0){
+//             return a;
+//         }
+//         return gcd(b,a%b);
+//     }
+
+//     static int max(int[] arr){
+//         int max=arr[0];
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]>max){
+//             max=arr[i];
+//             }
+//         }
+//         return max;
+//     }
+
+//     static int min(int[] arr){
+//         int min=arr[0];
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i]<min){
+//             min=arr[i];
+//             }
+//         }
+//         return min;
+//     }
+// }

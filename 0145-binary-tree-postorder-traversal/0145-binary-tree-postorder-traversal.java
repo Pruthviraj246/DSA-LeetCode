b49a7a399401937d5f26626a1cd11b1ef4a8ad11
Pruthviraj <pruthvirajmurthy79@gmail.com>
2026-09-17 21:11:41 +0530

    Time: 1 ms (12.2%), Space: 43.5 MB (15.44%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,20 +13,44 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> list=new ArrayList<>();
+//         if(root==null){
+//             return list;
+//         }
+//         List<Integer> left=postorderTraversal(root.left);
+        
+//         List<Integer> right=postorderTraversal(root.right);
+
+//         list.addAll(left);
+//         list.addAll(right);
+//         list.add(root.val);
+        
+//         return list;
+//     }
+// }
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
+
         if(root==null){
             return list;
         }
-        List<Integer> left=postorderTraversal(root.left);
-        
-        List<Integer> right=postorderTraversal(root.right);
+        st1.push(root);
+        while(!st1.isEmpty()){
+            root=st1.pop();
+            st2.add(root);
+            if(root.left!=null) st1.push(root.left);
+            if(root.right!=null) st1.push(root.right);
+        }
+        while(!st2.isEmpty()){
+            list.add(st2.pop().val);
+        }
 
-        list.addAll(left);
-        list.addAll(right);
-        list.add(root.val);
-        
         return list;
     }
 }

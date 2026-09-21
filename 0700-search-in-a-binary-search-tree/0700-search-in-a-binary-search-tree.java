c166621e7702a8260a313e5e1e7e3869a1b79eb4
Pruthviraj <pruthvirajmurthy79@gmail.com>
2026-09-21 09:44:49 +0530

    Time: 0 ms (100%), Space: 47.2 MB (16.72%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -13,28 +13,28 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode searchBST(TreeNode root, int val) {
-//         if(root==null){
-//             return null;
-//         }
-//         if(root.val==val){
-//             return root;
-//         }
-//         if(val>root.val){
-//             return searchBST(root.right,val);
-//         }else{
-//             return searchBST(root.left,val);
-//         }
-        
-//     }
-// }
-
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-       while(root!=null && root.val!=val){
-        root=val<root.val ? root.left:root.right;
-       }
-       return root;  
+        if(root==null){
+            return null;
+        }
+        if(root.val==val){
+            return root;
+        }
+        if(val>root.val){
+            return searchBST(root.right,val);
+        }else{
+            return searchBST(root.left,val);
+        }
+        
     }
 }
+
+// class Solution {
+//     public TreeNode searchBST(TreeNode root, int val) {
+//        while(root!=null && root.val!=val){
+//         root=val<root.val ? root.left:root.right;
+//        }
+//        return root;  
+//     }
+// }
